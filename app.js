ca93ace8d35ff46df9ba34dcc1ddbd1d6da2fda9
shz-cod <web.shahidul.alam@gmail.com>
2023-06-10 23:13:10 +0600

    Added js and css files

---
+++ app.js
@@ -0,0 +1,9 @@
+// Your solution goes here
+// Hints:
+// 1. Assign a id to the tags you want to select
+// 2. Use appropriate methods and properties to complete the specified task.
+// Example: 
+// (style) property to change style
+// (classList) property to modify class
+// (getAttribute) and (setAttribute) to modify attributes
+
