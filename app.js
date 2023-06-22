55a3715f395cd9bc04dc7557cc80962d2ddd0597
shz-cod <web.shahidul.alam@gmail.com>
2023-06-23 02:21:07 +0600

    final

--- app.js
+++ app.js
@@ -21,6 +21,36 @@ const books = [
   },
 ];
 
+// Dynamic individual bookItem
+function bookItem(book) {
+  return `
+    <div class="book col-lg-6 mb-4">
+    <div class="card mb-3">
+      <div class="row g-0">
+        <div class="col-md-3 book_img">
+          <img
+            src="${book.thumbnail}"
+            class="img-fluid rounded-start"
+            alt="..."
+          />
+        </div>
+        <div class="col-md-9">
+          <div class="card-body text-center text-md-start">
+            <h5 class="card-title">${book.name}</h5>
+            <p class="card-text">
+            ${book.desc}
+            </p>
+            <p class="card-text">${book.author}</p>
+            <p class="card-text">${book.price}</p>
+            <p class="card-text"><small class="opacity-50">${book.rating} /5</small></p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+    `;
+}
+
 // Select all the required DOM elements
 const allBooks = document.querySelector(".all_books");
 const searchForm = document.querySelector("#search-form");
@@ -35,83 +65,63 @@ const desc = document.querySelector("#desc");
 const price = document.querySelector("#price");
 const rating = document.querySelector("#rating");
 
-// Dynamic individual bookItem
-function bookItem(book) {
-  return `
-<div class="book col-lg-6 mb-4">
-    <div class="card mb-3">
-      <div class="row g-0">
-        <div class="col-md-3 book_img">
-          <img
-            src="${book.thumbnail}"
-            class="img-fluid rounded-start"
-            alt="..."
-          />
-        </div>
-        <div class="col-md-9">
-          <div class="card-body text-center text-md-start">
-            <h5 class="card-title">${book.name}</h5>
-            <p class="card-text">
-              ${book.desc}
-            </p>
-            <p class="card-text">${book.author}</p>
-            <p class="card-text">${book.price} Taka</p>
-            <p class="card-text"><small class="opacity-50">${book.rating}/5</small></p>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-  `;
-}
+let searchValueLocal = "";
 
 /*
 renderBooks function renders all the books to the DOM
 it is also used to filter out books and show conditional msg
 if no book is found.
 */
-function renderBooks(val) {
+function renderBooks() {
   allBooks.innerHTML = "";
   books.map(function (book) {
-    if (book.name.toLowerCase().includes(val.toLowerCase())) {
+    if (
+      book.name
+        .toLocaleLowerCase()
+        .includes(searchValueLocal.toLocaleLowerCase())
+    ) {
       allBooks.innerHTML += bookItem(book);
     }
   });
   if (allBooks.innerHTML === "")
     allBooks.innerHTML = `<span class="bg-danger text-white py-2 rounded">Nothing Found</span>`;
 }
-
 // renderBooks initial call
-renderBooks("");
-
-// Search functionality
-searchForm.addEventListener("submit", function () {
-  event.preventDefault();
-  let val = searchVal.value;
-  renderBooks(val);
-  searchVal.value = "";
-});
+renderBooks();
 
 // View showBooks block
 function activeShowBooks() {
-  showBooks.style.display = "block"; // Hiding showBook block
+  showBooks.style.display = "block";
   showForm.style.display = "none";
 }
-
 // View addNewBook block
 function activeShowForm() {
   showBooks.style.display = "none";
   showForm.style.display = "block";
 }
+// Initial call
+activeShowBooks();
+
+// Search functionality
+searchForm.addEventListener("submit", function (e) {
+  e.preventDefault();
+  searchValueLocal = searchVal.value;
+  // searchVal.value = "";
+  renderBooks();
+});
 
 // Add new book form
 /*
   here (name) is DOM input element and (name.value) is the value written in input field
 */
-addNewBookForm.addEventListener("submit", function () {
-  event.preventDefault();
-  let lastId = books[books.length - 1].id; // Getting id of previous book
-  books.push({
+addNewBookForm.addEventListener("submit", function (e) {
+  e.preventDefault();
+  let lastObj = books[books.length - 1];
+  let lastId = 0;
+  if (lastObj != undefined) {
+    lastId = lastObj.id;
+  }
+  let newObj = {
     id: lastId + 1,
     name: name.value,
     author: author.value,
@@ -119,7 +129,8 @@ addNewBookForm.addEventListener("submit", function () {
     desc: desc.value,
     price: price.value,
     rating: rating.value,
-  }); // Adding new book
-  renderBooks(""); // After adding new book rerendering DOM
+  };
+  books.push(newObj); // Adding new book
+  renderBooks(); // After adding new book rerendering DOM
   activeShowBooks(); // After adding new book change to showBooks view
 });
