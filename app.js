df8e036bb455757c2b3bb4101d47ffa541454268
shz-code <web.shahidul.alam@gmail.com>
2023-07-12 22:54:39 +0600

    api

--- app.js
+++ app.js
@@ -1,55 +1,6 @@
-const books = [
-  {
-    name: "Slow Horses (Deluxe Edition)",
-    author: "Mick Herron",
-    thumbnail:
-      "https://m.media-amazon.com/images/I/51Ga5GuElyL._SX331_BO1,204,203,200_.jpg",
-    desc: "Lorem",
-    price: 140,
-    rating: 3,
-    id: 1,
-  },
-  {
-    name: "The Last Thing He Told Me: A Novel",
-    author: "Laura Dave",
-    desc: "Lorem",
-    thumbnail:
-      "https://m.media-amazon.com/images/P/1501171348.01._SCLZZZZZZZ_SX500_.jpg",
-    price: 13.99,
-    rating: 2,
-    id: 2,
-  },
-];
+// https://64a5b50300c3559aa9c01831.mockapi.io/api/books/book
 
-// Dynamic individual bookItem
-function bookItem(book) {
-  return `
-    <div class="book col-lg-6 mb-4">
-    <div class="card mb-3">
-      <div class="row g-0">
-        <div class="col-md-3 book_img">
-          <img
-            src="${book.thumbnail}"
-            class="img-fluid rounded-start"
-            alt="..."
-          />
-        </div>
-        <div class="col-md-9">
-          <div class="card-body text-center text-md-start">
-            <h5 class="card-title">${book.name}</h5>
-            <p class="card-text">
-            ${book.desc}
-            </p>
-            <p class="card-text">${book.author}</p>
-            <p class="card-text">${book.price}</p>
-            <p class="card-text"><small class="opacity-50">${book.rating} /5</small></p>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-    `;
-}
+const books = [];
 
 // Select all the required DOM elements
 const allBooks = document.querySelector(".all_books");
@@ -65,29 +16,73 @@ const desc = document.querySelector("#desc");
 const price = document.querySelector("#price");
 const rating = document.querySelector("#rating");
 
-let searchValueLocal = "";
+let searchParam = "";
 
-/*
-renderBooks function renders all the books to the DOM
-it is also used to filter out books and show conditional msg
-if no book is found.
-*/
-function renderBooks() {
-  allBooks.innerHTML = "";
-  books.map(function (book) {
-    if (
-      book.name
-        .toLocaleLowerCase()
-        .includes(searchValueLocal.toLocaleLowerCase())
-    ) {
-      allBooks.innerHTML += bookItem(book);
-    }
-  });
-  if (allBooks.innerHTML === "")
-    allBooks.innerHTML = `<span class="bg-danger text-white py-2 rounded">Nothing Found</span>`;
+// Dynamic individual bookItem
+function bookItem(book) {
+  return `
+      <div class="book col-lg-6 mb-4">
+      <div class="card mb-3">
+        <div class="row g-0">
+          <div class="col-md-3 book_img">
+            <img
+              src="${book.thumbnail}"
+              class="img-fluid rounded-start"
+              alt="..."
+            />
+          </div>
+          <div class="col-md-9">
+            <div class="card-body text-center text-md-start">
+              <h5 class="card-title">${book.name}</h5>
+              <p class="card-text">
+              ${book.desc}
+              </p>
+              <p class="card-text">${book.author}</p>
+              <p class="card-text">${book.price}</p>
+              <p class="card-text"><small class="opacity-50">${book.rating} /5</small></p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+      `;
 }
-// renderBooks initial call
-renderBooks();
+
+function renderBooks() {
+  // console.log(books);
+    allBooks.innerHTML = "";
+    books.forEach(function (book) {
+        if(book.name.toLowerCase().includes(searchParam.toLowerCase()))
+        {
+            allBooks.innerHTML += bookItem(book);
+        }
+      });
+}
+
+
+async function getBooks() {
+  allBooks.innerHTML = "Loading.....";
+
+  let url = "https://64a5b50300c3559aa9c01831.mockapi.io/api/books/book/6";
+  let option = {
+    method: "GET"
+  }
+  try {
+    let response = await fetch(url,option);
+    let data = await response.json();
+    console.log(data);
+    // data.forEach(function(current){
+    //   books.push(current);
+    // });
+    renderBooks();
+  }
+  catch (err) {
+    console.log(err);
+  }
+}
+
+getBooks();
+
 
 // View showBooks block
 function activeShowBooks() {
@@ -99,16 +94,26 @@ function activeShowForm() {
   showBooks.style.display = "none";
   showForm.style.display = "block";
 }
-// Initial call
+
 activeShowBooks();
 
 // Search functionality
 searchForm.addEventListener("submit", function (e) {
-  e.preventDefault();
-  searchValueLocal = searchVal.value;
-  // searchVal.value = "";
-  renderBooks();
-});
+    e.preventDefault();
+    searchParam = searchVal.value;
+    renderBooks();
+  });
+
+async function postBook(newObj) {
+  let url = "https://64a5b50300c3559aa9c01831.mockapi.io/api/books/book";
+  let option = {
+    method: "POST",
+    headers: {'content-type':'application/json'},
+    body: JSON.stringify(newObj)
+  }
+  let response = await fetch(url,option);
+  console.log(response);
+}
 
 // Add new book form
 /*
@@ -131,6 +136,7 @@ addNewBookForm.addEventListener("submit", function (e) {
     rating: rating.value,
   };
   books.push(newObj); // Adding new book
+  postBook(newObj);
   renderBooks(); // After adding new book rerendering DOM
   activeShowBooks(); // After adding new book change to showBooks view
 });
